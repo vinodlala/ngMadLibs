0717e8110cfb9b623c4c7124840668d96635ddb5
Vinod Lala <vinod_lala@usa.net>
2015-05-14 18:57:49 -0400

    Validate, switch between views

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-angular.module('myApp', [])
-    .controller("MyCtrl", function($scope) {
+angular.module('myApp', ['ngMessages'])
+    .controller("MyCtrl", function ($scope) {
+
+        $scope.switchToOutput = false;
+
+        $scope.generateMadLib = function () {
+
+            if ($scope.myForm.$valid) {
+                $scope.switchToOutput = true;
+            } else {
+                $scope.switchToOutput = false;
+            }
+        };
+
+        $scope.startOver = function () {
+            $scope.switchToOutput = false;
+            $scope.gender = "";
+            $scope.firstName = "";
+            $scope.jobTitle = "";
+            $scope.tediousTask = "";
+            $scope.dirtyTask = "";
+            $scope.celebrity = "";
+            $scope.uselessSkill = "";
+            $scope.adjective = "";
+            $scope.obnoxiousCelebrity = "";
+            $scope.hugeNumber = "";
+            $scope.myForm.$setPristine();
+            $scope.myForm.$setUntouched();
+        };
 
     });
